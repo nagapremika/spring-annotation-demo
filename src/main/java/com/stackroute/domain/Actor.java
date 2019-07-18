ea374e66dbad32734952176e714a38dfc80e4f00
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-07-18 22:46:27 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class Actor {
     @Value("Vijay")
-    public void setName(String name) {
+    public void setName(String name) {      //sets name as Vijay
         this.name = name;
     }
 
     @Value("Male")
-    public void setGender(String gender) {
+    public void setGender(String gender) {      //SETS gender male
         this.gender = gender;
     }
 
     @Value("24")
-    public void setAge(int age) {
+    public void setAge(int age) {       //sets age 24
         this.age = age;
     }
     public void display()
