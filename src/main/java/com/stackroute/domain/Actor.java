37d072ad2ccfd5c5c46adb30f123f868599a3faa
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-07-18 23:01:57 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,11 +1,11 @@
 package com.stackroute.domain;
 
-public class Actor {
+public class Actor {    
     private String name;
     private String gender;
     private int age;
 
-    public Actor(String name, String gender, int age) {
+    public Actor(String name, String gender, int age) { //Constructor
         this.name = name;
         this.gender = gender;
         this.age = age;
