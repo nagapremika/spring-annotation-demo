d417f74b58c398c5662f85d529b607d68d050b52
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-07-18 23:03:19 +0530

    Update Config.java

--- src/main/java/com/stackroute/domain/Config.java
+++ src/main/java/com/stackroute/domain/Config.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
     @Bean
-    public Actor getActor1()
+    public Actor getActor1()    //Actor Bean
     {
         return new Actor("vijay","male",24);
     }
 
     @Bean
-    public Movie getMovie1()
+    public Movie getMovie1()        //Movie Bean
     {
         Movie movie1=new Movie(getActor1());
         movie1.setActor(getActor1());
